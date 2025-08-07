102983bba55abf8300527c3ecb533d78c09bd384
giolajide <141545135+giolajide@users.noreply.github.com>
2025-08-07 16:23:44 -0500

    Update create_spherical_caps.py

--- nanoparticleatomcounting/tests/compare_to_atomistic/create_spherical_caps.py
+++ nanoparticleatomcounting/tests/compare_to_atomistic/create_spherical_caps.py
@@ -7,7 +7,7 @@ from ase.build import bulk
 import warnings
 from ase.data import chemical_symbols
 from typing import Union, Optional, List, Tuple
-from nanoparticleatomcounting.tests.atomistic_utils import scaler, create_unit_support
+from nanoparticleatomcounter.tests.atomistic_utils import scaler, create_unit_support
 #from functools import lru_cache
 
 
