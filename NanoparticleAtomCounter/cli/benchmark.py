f10c7c7b5ad031816cdc32db482cf5355e8c76f7
giolajide <141545135+giolajide@users.noreply.github.com>
2025-09-30 07:19:53 -0500

    Update benchmark.py

--- NanoparticleAtomCounter/cli/benchmark.py
+++ NanoparticleAtomCounter/cli/benchmark.py
@@ -6,13 +6,11 @@ from pathlib import Path
 import sys
 from ascii_colors import ASCIIColors
 #import pytest
-from benchmark.benchmark import main as benchmarking
+from benchmark.test_atomcount import main as benchmarking
 
 def main() -> None:
     """
-    Run the test_atomcount.py script,
-    which benchmarks,
-    then exit with pytest’s status code.
+    Run the test_atomcount.py script, which benchmarks
     """
     print("This should take about 5 minutes,\n depending on how many processors you have . . .\n\n")
 #    repo_root = Path(__file__).resolve().parents[2]
